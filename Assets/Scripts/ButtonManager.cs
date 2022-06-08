aea42c803ae7deff6801a25982cf76a7a040ea42
hasanvurucu <47665574+hasanvurucu@users.noreply.github.com>
2022-06-08 15:09:45 +0300

    Initial Commit

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject CapsuleBot;
+    [SerializeField]
+    private Text stateButtonText;
+
+    public void ChangeState()
+    {
+        var botScript = CapsuleBot.GetComponent<CommandControlBot>();
+        if (botScript.botState == BotState.QUEUE)
+        {
+            botScript.botState = BotState.STANDART;
+            stateButtonText.text = "Current State:\nSTANDART";
+        }
+        else if (botScript.botState == BotState.STANDART)
+        {
+            botScript.botState = BotState.QUEUE;
+            stateButtonText.text = "Current State:\nQUEUE";
+        }
+    }
+}
---
+++ Assets/Scripts/CommandControlBot.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum BotState
+{
+    STANDART,
+    QUEUE
+}
+
+public class CommandControlBot : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private Queue<Command> commands = new Queue<Command>();
+    private Command currentCommand;
+
+    public BotState botState;
+
+    private void Awake() => botState = BotState.STANDART;
+    private void Start() => agent = GetComponent<NavMeshAgent>();
+
+    private void Update()
+    {
+        ListenForCommands();
+        ProcessCommands();
+    }
+
+    private void ListenForCommands()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (botState == BotState.QUEUE)
+            {
+                while(commands.Count > 0)
+                {
+                    commands.Dequeue();
+                }
+            }
+             agent.SetDestination(transform.position);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out var hitInfo))
+            {
+                if(botState == BotState.STANDART)
+                {
+                    agent.SetDestination(hitInfo.point);
+                }
+                else if(botState == BotState.QUEUE)
+                {
+                    var moveCommand = new MoveCommand(hitInfo.point, agent);
+                    commands.Enqueue(moveCommand);
+                }
+            }
+        }
+        
+    }
+    
+    private void ProcessCommands()
+    {
+        if (currentCommand != null && currentCommand.IsFinished == false)
+            return;
+
+        if (commands.Count == 0)
+            return;
+
+        currentCommand = commands.Dequeue();
+        currentCommand.Execute();
+    }
+
+    internal class MoveCommand: Command
+    {
+        private readonly Vector3 _destination;
+        private readonly NavMeshAgent agent;
+
+        public MoveCommand(Vector3 destination, NavMeshAgent agent)
+        {
+            _destination = destination;
+            this.agent = agent;
+        }
+
+        public override void Execute()
+        {
+            agent.SetDestination(_destination);
+        }
+
+        public override bool IsFinished => agent.remainingDistance < 0.1f;
+    }
+}
